56b2ac75df6867d84e06cca867741c44204bcac9
pipi-shortstocking <jannie435@naver.com>
2025-01-12 00:52:15 +0900

    fix: 로그인 시, 역할 전달(#22)

--- src/main/java/pepperstone/backend/user/dto/response/TokenResponseDTO.java
+++ src/main/java/pepperstone/backend/user/dto/response/TokenResponseDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenResponseDTO {
+    private String userRole;
     private String accessToken;
     private String refreshToken;
 }
